36a2251396a1c1d99552a24a31bdb2f18a24f339
hasnainhakim <108686425+hasnainhakim@users.noreply.github.com>
2023-01-21 23:21:02 +0500

    script commit

---
+++ Script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Input file with the list of S3 buckets and AWS accounts
+input_file="input.csv"
+
+# Output file with the size of the S3 buckets
+output_file="output.csv"
+
+# Create the output file with the header
+echo "Bucket,Size (GB)" > $output_file
+
+# Read the input file line by line
+while IFS=',' read -r bucket account
+do
+  # Get the size of the S3 bucket using the AWS CLI
+  size=$(aws --profile $account s3 ls s3://$bucket --recursive | awk 'BEGIN {total=0}{total+=$3}END{print total/1024/1024/1024}')
+
+  # Append the size of the S3 bucket to the output file
+  echo "$bucket,$size" >> $output_file
+done < $input_file
