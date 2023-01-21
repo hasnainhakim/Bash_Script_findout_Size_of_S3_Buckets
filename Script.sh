3fac69a08374fd11070b4804d5088d9975472d6a
hasnainhakim <108686425+hasnainhakim@users.noreply.github.com>
2023-01-21 23:22:11 +0500

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -7,7 +7,7 @@ input_file="input.csv"
 output_file="output.csv"
 
 # Create the output file with the header
-echo "Bucket,Size (GB)" > $output_file
+echo "Bucket,Account,Size (GB)" > $output_file
 
 # Read the input file line by line
 while IFS=',' read -r bucket account
@@ -15,6 +15,6 @@ do
   # Get the size of the S3 bucket using the AWS CLI
   size=$(aws --profile $account s3 ls s3://$bucket --recursive | awk 'BEGIN {total=0}{total+=$3}END{print total/1024/1024/1024}')
 
-  # Append the size of the S3 bucket to the output file
-  echo "$bucket,$size" >> $output_file
+  # Append the bucket name, account and size of the S3 bucket to the output file
+  echo "$bucket,$account,$size" >> $output_file
 done < $input_file
